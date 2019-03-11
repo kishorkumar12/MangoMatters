05d9646ee57cac29e323f12c4ef118912ca54249
Vishalakshi N <vishalakshi@prakat.in>
2019-03-11 14:39:42 +0530

    create test controller

--- src/main/java/com/prkat/mangomatters/TestController.java
+++ src/main/java/com/prkat/mangomatters/TestController.java
@@ -1,4 +1,20 @@
 package com.prkat.mangomatters;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+    String name ="All Mangoes are sold now,Wait for few minites";
+
+    @GetMapping("/mangoes")
+    public String myMessage(String m) {
+
+        return "ShowListofMangos";
+
+
+    }
+
+
 }
